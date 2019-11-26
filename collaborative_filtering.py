1d34205ed0fd89cba0700282b2c921f90cd0214b
Anusha Lihala <35848893+anushalihala@users.noreply.github.com>
2019-11-26 00:11:35 +0000

    Add files via upload

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -17,8 +17,10 @@ def collaborative_filtering(user_id, num_of_predictions, recipe_history=[]):
     Vt = pickle.load(fh)
   with open("sigma.pickle","rb") as fh:
     sigma = pickle.load(fh)
-  with open("column_recipe_ids.pickle","rb") as fh:
+  with open("column_recipe_indices.pickle","rb") as fh:
     column_recipe_ids = pickle.load(fh)
+  with open("recipe_id_dict.pickle","rb") as fh:
+    recipe_id_dict = pickle.load(fh)
     
   user_vector = U[user_id]
     
@@ -27,7 +29,11 @@ def collaborative_filtering(user_id, num_of_predictions, recipe_history=[]):
   pred_indices = np.argsort(pred_values)
   pred_recipe_ids = column_recipe_ids[pred_indices]
   pred_recipe_ids = np.array([id for id in pred_recipe_ids if id not in recipe_history])
-  return pred_recipe_ids[-num_of_predictions:]
+  return [recipe_id_dict[int(idx)] for idx in pred_recipe_ids[-num_of_predictions:]]
   
 # #testing
-# print(collaborative_filtering(0, 10))
+with open("recipe_history_0.pickle","rb") as fh:
+  recipe_history_0 = pickle.load(fh)
+ 
+print(collaborative_filtering(0, 10, recipe_history_0))
+print(collaborative_filtering(0, 10))
